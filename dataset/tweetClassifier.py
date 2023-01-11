6c1a97ed78e32328c37962c21f660bb0159fe16f
neeraja-m <nm18101@gmail.com>
2023-01-11 02:03:57 +0000

    all done

--- dataset/tweetClassifier.py
+++ dataset/tweetClassifier.py
@@ -7,11 +7,25 @@ import category_encoders as cat_encoder
 import seaborn as sns
 import plotly.express as px
 from langdetect import detect
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.svm import SVC
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
+
+from sklearn.svm import LinearSVC
+from sklearn import svm
 from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
 import re
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.metrics import accuracy_score
+import sklearn.metrics as metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
 # ----------------------------------------------   import datasets   ---------------------------------------------- #
 
 csv_table=pd.read_table('mediaeval-2015-trainingset.txt',sep='\t')
@@ -30,11 +44,15 @@ test= pd.read_csv("test.csv")
 training_data.dropna(axis='index',how='any')
 
 # convert ground truth labels
-ground_truth_codes = {'fake':'fake','humor':'fake', 'real':'real'}
+ground_truth_codes = {'fake':0,'humor':0, 'real':1}
 training_data['label'] = training_data['label'].map(ground_truth_codes)
 
 # remove imageId
 training_data = training_data.drop('imageId(s)',axis=1) 
+
+# oversampling to fix class imbalance
+overSampler = RandomOverSampler(random_state=0)
+new_training_data, new_training_labels = overSampler.fit_resample(training_data, training_data['label'])
 
 # function to clean tweetText data itself
 def clean_tweetText(textToClean):
@@ -61,65 +79,100 @@ def find_language_count(textToCheck):
 
     return lang
 
-training_data['language'] = training_data['tweetText'].apply(find_language_count)
-training_data['tweetText'] = training_data['tweetText'].apply(clean_tweetText)
 
-print(training_data.head())
+#function to remove stopwords in each tweetText
+stopword_list = stopwords.words('english')
 
-#removing stopwords
-# stopword_list = stopwords.words('english')
-# training_data['tweetText'] = training_data['tweetText'].apply(lambda x: ' '.join([wordToCheck for wordToCheck in x.split() if wordToCheck not in (stopword_list)]))
+def remove_stopwords(textToCheck):
+    return ' '.join([wordToCheck for wordToCheck in textToCheck.split() if wordToCheck not in (stopword_list)])
 
-# -------------------------------------------  data visuaisation   -------------------------------------------- #
+
+# apply text cleaning functions
+new_training_data['language'] = new_training_data['tweetText'].apply(find_language_count)
+new_training_data['tweetText'] = new_training_data['tweetText'].apply(clean_tweetText)
+new_training_data['tweetText'] = new_training_data['tweetText'].apply(remove_stopwords)
+
+# print(new_training_data.head(10))
+
+# -------------------------------------------  data visualisation  -------------------------------------------- #
 
 # plot number of posts in each langauge -----------> change this to pie chart
-# lang_plot = sns.countplot(x="language", hue="label", data=training_data,palette='Set2')
+# lang_plot = sns.countplot(x="language", hue="label", data=new_training_data,palette='Set2')
 # plt.show()
 
-# plot number of posts in each class
+# plot number of posts in each class for original dataset
 # training_data.groupby(['label'])['tweetText'].count().plot(kind='bar')
 # label_plot = sns.countplot(x="label", data=training_data,palette='Set2')
 # plt.show()
 
-# plot tweet lenghts?
+# plot number of posts in each class for oversampled dataset
+# new_training_data.groupby(['label'])['tweetText'].count().plot(kind='bar')
+# label_plot = sns.countplot(x="label", data=new_training_data,palette='Set2')
+# plt.show()
 
 
-# plot 
+# plot tweet lengths
+
+
+# plot common words
 
 
 
 # --------------------------------------------   clean test data   -------------------------------------------- #
-# testing_data['tweetText'] = testing_data['tweetText'].apply(clean_tweetText)
+# test data will not be oversampled
+
+# remove NA values
+testing_data.dropna(axis='index',how='any')
+
+# convert ground truth labels
+ground_truth_codes = {'fake':0,'humor':0, 'real':1}
+testing_data['label'] = testing_data['label'].map(ground_truth_codes)
+
+# remove imageId
+testing_data = testing_data.drop('imageId(s)',axis=1) 
+
+# apply text cleaning functions
+testing_data['tweetText'] = testing_data['tweetText'].apply(clean_tweetText)
+testing_data['tweetText'] = testing_data['tweetText'].apply(remove_stopwords)
 
 # ----------------------------------------------  classifier  ------------------------------------------------- #
+training_text = new_training_data['tweetText']
+training_label = new_training_data['label']
+testing_text = testing_data['tweetText']
+testing_label = testing_data['label']
 
-# cv = CountVectorizer(max_features = 5000)
-# X = cv.fit_transform(corpus).toarray()
-# y = training_data.iloc[0:40000, 2].values
+# apply tokenising
+count_vectoriser = CountVectorizer(stop_words='english')
+count_training_text = count_vectoriser.fit_transform(training_text)
+count_testing_text = count_vectoriser.transform(testing_text)
 
-# classifier = SVC(kernel = 'linear', random_state = 0)
-# classifier.fit(training_data, testing_data)
-
-# # Predicting the Test set results
-# test_prediction = classifier.predict(testing_data)
+# train classifier
+naive_bayes = MultinomialNB()
+naive_bayes.fit(count_training_text, training_label)
+test_data_prediction = naive_bayes.predict(count_testing_text)
 
 # --------------------------------------------  result analysis  ----------------------------------------------- #
 
-# Precision
-
-
-
 # Recall
+r_score = metrics.recall_score(testing_label, test_data_prediction)
+print("Recall:   %0.3f" % r_score)
 
-
+# Precision
+p_score = metrics.precision_score(testing_label, test_data_prediction)
+print("Precision:   %0.3f" % p_score)
 
 # F1 Score
+f_score = metrics.f1_score(testing_label, test_data_prediction)
+print("F1 score:   %0.3f" % f_score)
 
-
+# Accuracy
+a_score = metrics.accuracy_score(testing_label, test_data_prediction)
+print("Accuracy:   %0.3f" % a_score)
 
 # Confusion Matrix
-# confusion_matrix = confusion_matrix(testing_data, test_prediction)
-# my_df = pd.DataFrame(training_data)
+# conf_matrix = confusion_matrix(testing_data, test_data_prediction)
+
+# print(cm5)
 # sns.heatmap(my_df.corr())
-# # print(confusion_matrix)
+# print(conf_matrix)
 
